f420f85a102776d6f435f86775ff0cb1521c3f44
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-11-25 21:04:58 -0300

    Page Sign in

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,9 @@ export default class MyDocument extends Document{
     return(
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonst.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main/>
