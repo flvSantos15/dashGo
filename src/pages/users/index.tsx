172d6343c574f9b774b6f9c62c2a15682343d282
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-11-27 06:45:00 -0300

    component pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import Header from "../../components/Header";
+import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UsersList(){
@@ -104,6 +105,9 @@ export default function UsersList(){
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination/>
+
         </Box>
       </Flex>
     </Box>
