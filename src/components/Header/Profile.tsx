6c511fbc76b86061e43c226e6d58b878828901d5
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-11-28 21:45:03 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,20 @@
-import {Flex, Box, Text, Avatar} from '@chakra-ui/react'
+import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile(){
-  return(
-    <Flex
-      align='center'
-    >
-      <Box mr='4' textAlign='right'>
-        <Text>Flavio Santos</Text>
-        <Text color='gray.300' fontSize='small'>flvSantos300@gmail.com</Text>
-      </Box>
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-      <Avatar size='md' name='Flavio Santos' src='http://github.com/flvSantos15.png'/>
+export function Profile({ showProfileData }: ProfileProps) {
+  return (
+    <Flex align='center'>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Flavio Santos</Text>
+          <Text color='gray.300' fontSize='small'>flvSantos300@gmail.com</Text>
+        </Box>
+      )}
+
+      <Avatar size='md' name='Flavio Santos' src='http://github.com/flvSantos15.png' />
     </Flex>
   )
 }
