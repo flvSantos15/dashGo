9d1c109d954bfdbd10e4c3e73325b43de19e975d
Flavio Santos <flvsantos300@gmail.com>
2022-01-03 08:53:19 -0300

    utilizando mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,10 +12,14 @@ import Link from 'next/link'
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { useMutation } from 'react-query'
+import { useRouter } from "next/router";
 
 import { Input } from "../../components/Form/Input";
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -34,16 +38,32 @@ const createUserFormSchema = yup.object({
 })
 
 export default function CreateUser() {
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+    return response.data.user
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  })
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
 
   const { errors } = formState
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (value) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values)
 
-    console.log(value)
+    router.push('/users')
   }
   return (
     <Box>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -36,7 +36,7 @@ export default function UsersList() {
     lg: true,
   })
 
-  async function handlePrefecthUser(userId: number){
+  async function handlePrefecthUser(userId: string){
     await queryClient.prefetchQuery(['user', userId], async () => {
       const response = await api.get(`users/${userId}`)
 
